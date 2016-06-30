92081880d00f98fe737ff5fc797679425fb7e862
Christian Vazquez <Pununuk@MacBook-Pro-de-Christian.local>
2016-06-29 21:18:00 -0300

    agrego filtros y tabla

--- js/usuario-consulta.js
+++ js/usuario-consulta.js
@@ -18,4 +18,24 @@
       $('.easy-sidebar-container').removeClass('toggled');
       $('.navbar.easy-sidebar').removeClass('toggled');
     });
+
+  $('.easy-sidebar .btn-primary')
+    .on('click', function(){
+      $('.easy-sidebar .btn-primary').removeClass('btn-selected-primary');
+      $(this).addClass('btn-selected-primary');
+    });
+
+  $('.easy-sidebar .btn-secondary')
+    .on('click', function(){
+      $('.easy-sidebar .btn-secondary').removeClass('btn-selected-secondary');
+      $(this).addClass('btn-selected-secondary');
+    });
+
+    $(function() {
+      $('input[name="daterange"]').daterangepicker();
+    });
+
+    $(document).ready(function() {
+      $('#example').DataTable();
+    });
 })();
