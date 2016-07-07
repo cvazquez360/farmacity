270c089b56d0473c207f1811e522e7ec154db9c7
Christian Vazquez <Pununuk@MacBook-Pro-de-Christian.local>
2016-07-07 16:49:28 -0300

    Quitando outline btn

--- js/usuario-consulta.js
+++ js/usuario-consulta.js
@@ -48,7 +48,70 @@
             "info": "Página _PAGE_ de _PAGES_",
             "infoEmpty": "No se encontraron registros",
             "infoFiltered": "(filtered from _MAX_ total records)",
+          "paginate": {
+             "next": "Siguiente",
+             "previous": "Anterior"
+         },
         }
       })
     });
+
 })();
+
+function animateCollapsibles() {
+  $('#filtros').on('hidden.bs.collapse', function () {
+      $('.button-collapse').find('.fa-chevron-right').css({
+          'moz-transform': 'rotate(0deg)',
+          '-webkit-transform': 'rotate(0deg)',
+          '-o-transform': 'rotate(0deg)',
+          '-ms-transform': 'rotate(0deg)',
+          'transform': 'rotate(0deg)',
+          '-webkit-transition': 'all .2s ease',
+          '-moz-transition': 'all .2s ease',
+          '-ms-transition': 'all .2s ease',
+          '-o-transition': 'all .2s ease',
+          'transition': 'all .2s ease'
+      });
+  }).on('show.bs.collapse', function () {
+      $('.button-collapse').find('.fa-chevron-right').css({
+          'moz-transform': 'rotate(90deg)',
+          '-webkit-transform': 'rotate(90deg)',
+          '-o-transform': 'rotate(90deg)',
+          '-ms-transform': 'rotate(90deg)',
+          'transform': 'rotate(90deg)',
+          '-webkit-transition': 'all .2s ease',
+          '-moz-transition': 'all .2s ease',
+          '-ms-transition': 'all .2s ease',
+          '-o-transition': 'all .2s ease',
+          'transition': 'all .2s ease'
+      });
+  })
+
+  $('#tabla').on('hidden.bs.collapse', function () {
+      $('.button-collapse').find('.fa-chevron-down').css({
+          'moz-transform': 'rotate(-90deg)',
+          '-webkit-transform': 'rotate(-90deg)',
+          '-o-transform': 'rotate(-90deg)',
+          '-ms-transform': 'rotate(-90deg)',
+          'transform': 'rotate(-90deg)',
+          '-webkit-transition': 'all .2s ease',
+          '-moz-transition': 'all .2s ease',
+          '-ms-transition': 'all .2s ease',
+          '-o-transition': 'all .2s ease',
+          'transition': 'all .2s ease'
+      });
+  }).on('show.bs.collapse', function () {
+      $('.button-collapse').find('.fa-chevron-down').css({
+          'moz-transform': 'rotate(0deg)',
+          '-webkit-transform': 'rotate(0deg)',
+          '-o-transform': 'rotate(0deg)',
+          '-ms-transform': 'rotate(0deg)',
+          'transform': 'rotate(0deg)',
+          '-webkit-transition': 'all .2s ease',
+          '-moz-transition': 'all .2s ease',
+          '-ms-transition': 'all .2s ease',
+          '-o-transition': 'all .2s ease',
+          'transition': 'all .2s ease'
+      });
+  })
+}
